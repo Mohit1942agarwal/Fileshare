606dec57462dfb6b0d17b2bdb735c3efae2b45ba
Mohit Agarwal <mohitagarwal@Mohits-MacBook-Air.local>
2024-11-02 18:26:51 +0530

    backend link corrected

--- FileSharing_frontend/index.js
+++ FileSharing_frontend/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://fastfile-file-sharing-app.onrender.com";
+const baseURL = "https://fileshare-backend-fj7z.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
